d2dd60c26956fd4a43bec7faae930f6a0c363da8
Joshua <mwalejoshua89@gmail.com>
2022-08-18 08:41:30 +0300

    Done the lab successfully

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,46 @@
 def admin_login(username, password)
-  # your code here
+  if ((username == "admin" || username == "ADMIN") && password == "12345")
+    "Access granted"
+  else
+    "Access denied"
 end
 
+
+response = "perfect"
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's a brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "a little chilly"
+  elsif temperature > 85
+    "too dang hot"
+  else 
+    "It's #{response} out there!"
 end
+
 
 def fizzbuzz(num)
-  # your code here
+if num % 3 == 0 && num % 5 == 0
+  return "Fizzbuzz"
+elsif num % 3 == 0
+  return "Fizz"
+elsif num % 5 == 0
+  return "Buzz"
+else 
+  return num
 end
 
+operation = "Invalid operation"
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    "Invalid operation"
 end
-
